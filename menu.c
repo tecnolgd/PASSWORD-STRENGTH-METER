b30f7d69480eac7f8bccfa2c217d549a5df2d1af
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-07-03 11:21:22 +0530

    Update : Add menu.c file for cli menu

---
+++ menu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu(int choice){
+    printf("\t$$ BANKING SYSTEM CLI $$\n");
+    printf("Menu :\n");
+    printf("\t1.");
+    printf("\t1.");
+    printf("\t1.");
+    printf("\t1.");
+  
+}
+
